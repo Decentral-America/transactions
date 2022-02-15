9b74e7b93cb8a4a7df1363b7148f1cd376820253
Gabo1122 <support@crcoinico.com>
2022-02-15 14:19:09 -0600

    update

--- test/integration/config.ts
+++ test/integration/config.ts
@@ -1,22 +1,22 @@
-import { randomBytes } from '@waves/ts-lib-crypto'
+import { address, privateKey, randomBytes } from '@waves/ts-lib-crypto';
 
 /**
  * Before running test ensure MASTER_SEED has at leas 10 DCC!!
  */
-// export const CHAIN_ID = 'S'
-// export const API_BASE = 'https://nodes-stagenet.wavesnodes.com' //3MVCPdW6ZUzLSmMj4RnpZKva1cnTdxQKtNt
+export const CHAIN_ID = 'S'
+export const API_BASE = 'https://nodes-stagenet.wavesnodes.com' //3MVCPdW6ZUzLSmMj4RnpZKva1cnTdxQKtNt
 
-export const CHAIN_ID = 'T'
-export const API_BASE = 'https://nodes-testnet.wavesnodes.com' //3MxhTY7cuPFJibGHcYogpviQipZdc5yeLCu
+// export const CHAIN_ID = 'T'
+// export const API_BASE = 'https://testnet-node.decentralchain.io'
 
+// export const MASTER_SEED = 'test acc 2'
 export const MASTER_SEED = 'test acc 3'
-// export const MASTER_SEED = 'next one puppy history bag vanish conduct lion royal dentist reject usual story invite leader'
-export const MATCHER_PUBLIC_KEY = '8QUAqtTckM5B8gvcuP7mMswat9SjKUuafJMusEoSn1Gy'
-export const MATCHER_URL = 'https://matcher-testnet.waves.exchange/'
+// console.log(address(MASTER_SEED, CHAIN_ID))
+export const MATCHER_PUBLIC_KEY = '2eEUvypDSivnzPiLrbYEW39SM8yMZ1aq4eJuiKfs4sEY'
+export const MATCHER_URL = 'https://matcher-stagenet.waves.exchange/'
 
 
 
 export const TIMEOUT = 60000
 
-// @ts-ignore
 export const randomHexString = (l: number) => [...randomBytes(l)].map(n => n.toString(16)).join('')
